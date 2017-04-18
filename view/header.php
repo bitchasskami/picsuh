877dde60c26103760b9056e03a52845c1ae8b0b3
Lüg Blaiger <luc.balsiger@bwdbern.ch>
2017-04-18 16:45:06 +0200

    Nav
    -

--- view/header.php
+++ view/header.php
@@ -3,6 +3,22 @@
     <head>
         <meta charset="utf-8">
         <link href="/css/style.css" ref="stylesheet">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     </head>
     <body>
-        
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">PicSuh</a>
+            </div>
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">User</a></li>
+                <li><a href="#">Gallery</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+            </ul>
+        </div>
+    </nav>
