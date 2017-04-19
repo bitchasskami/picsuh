35209520adb22fc9fc8b5ced45d51bace0a7c520
Kamiiiiiiiiiiiiiiiiiiiiiiii <legitkami@gmail.com>
2017-04-19 16:59:02 +0200

    Added blur effect
    Added a blur when you click on an image.

--- view/header.php
+++ view/header.php
@@ -2,8 +2,9 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <link href="/css/style.css" ref="stylesheet">
+        <link href="/css/style.css" rel="stylesheet">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
     </head>
     <body>
     <nav class="navbar navbar-default">
