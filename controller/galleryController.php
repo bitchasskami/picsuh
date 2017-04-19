cb144861b3e500af7f5c8ccdda0e29be092e264b
Kamiiiiiiiiiiiiiiiiiiiiiiii <legitkami@gmail.com>
2017-04-19 14:10:50 +0200

    added image upload and thumbnail generation
    You're now able to upload images. A thumbnail of them will be generated
    and shown on the 'Gallery' tab.

--- controller/galleryController.php
+++ controller/galleryController.php
@@ -1,7 +1,38 @@
 <?php
-class galleryController{
-    public function index(){
+class galleryController
+{
+    public function index()
+    {
         $view = new View('gallery_index');
         $view->display();
     }
+
+    function createThumbnail($filename) {
+        if(preg_match('/[.](jpg)$/', $filename)) {
+            $im = imagecreatefromjpeg('data/' . $filename);
+        } else if (preg_match('/[.](gif)$/', $filename)) {
+            $im = imagecreatefromgif('data/' . $filename);
+        } else if (preg_match('/[.](png)$/', $filename)) {
+            $im = imagecreatefrompng('data/' . $filename);
+        }
+
+        $ox = imagesx($im);
+        $oy = imagesy($im);
+
+        $nx = 150;
+        $ny = floor($oy * ($nx / $ox));
+
+        $nm = imagecreatetruecolor($nx, $ny);
+
+        imagecopyresized($nm, $im, 0,0,0,0,$nx,$ny,$ox,$oy);
+
+        if(!file_exists('data/thumbs')) {
+            if(!mkdir('data/thumbs')) {
+                die("There was a problem. Please try again!");
+            }
+        }
+
+        imagejpeg($nm, 'data/thumbs/' . $filename);
+    }
 }
+?>
--- view/gallery_index.php
+++ view/gallery_index.php
@@ -1,1 +1,67 @@
-suh gallery
+<?php
+
+if(isset($_FILES['image'])){
+
+    $errors= array();
+    $file_name = $_FILES['image']['name'];
+    $file_size = $_FILES['image']['size'];
+    $file_tmp = $_FILES['image']['tmp_name'];
+    $file_type = $_FILES['image']['type'];
+    $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+
+    $expensions= array("jpeg","jpg","png");
+
+    if(in_array($file_ext,$expensions)=== false){
+        $errors[]="extension not allowed, please choose a JPEG or PNG file.";
+    }
+
+    if($file_size > 2097152) {
+        $errors[]='File size must be excately 2 MB';
+    }
+
+    if(empty($errors)==true) {
+        $gallery = new galleryController();
+        move_uploaded_file($file_tmp,"data/".$file_name);
+        $gallery->createThumbnail($file_name);
+        echo "Success";
+    }else{
+        print_r($errors);
+    }
+}
+?>
+<html>
+<body>
+
+<?php
+$dir = 'data/thumbs/';
+$file_display = array('jpg', 'jpeg', 'png', 'gif');
+
+if ( file_exists( $dir ) == false ) {
+    echo 'Directory \'', $dir, '\' not found!';
+} else {
+    $dir_contents = scandir( $dir );
+
+    foreach ( $dir_contents as $file ) {
+        $file_type = strtolower( end( explode('.', $file ) ) );
+        if ( ($file !== '.') && ($file !== '..') && (in_array( $file_type, $file_display)) ) {
+            echo '<img src="'. $dir . '/' . $file.'" alt="', $file, '"/>';
+        }
+    }
+}
+?>
+
+<form action = "" method = "POST" enctype = "multipart/form-data">
+    <input type = "file" name = "image" />
+    <input type = "submit"/>
+
+    <ul>
+        <li>Sent file: <?php echo $_FILES['image']['name'];  ?>
+        <li>File size: <?php echo $_FILES['image']['size'];  ?>
+        <li>File type: <?php echo $_FILES['image']['type'] ?>
+    </ul>
+
+</form>
+
+</body>
+</html>
+
