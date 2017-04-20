ef775cddf7925c921f4a913306e760b3182e74d5
Lüg Blaiger <luc.balsiger@bwdbern.ch>
2017-04-20 13:02:57 +0200

    updated DB
    table 'gallery' added, corresponding ERM added in /data

--- data/createdb.sql
+++ data/createdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 19. Apr 2017 um 09:08
+-- Erstellungszeit: 20. Apr 2017 um 12:57
 -- Server-Version: 10.1.21-MariaDB
 -- PHP-Version: 5.6.30
 
@@ -23,15 +23,28 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Tabellenstruktur für Tabelle `gallery`
+--
+
+CREATE TABLE `gallery` (
+  `id` int(6) NOT NULL,
+  `user_id` int(6) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `description` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Tabellenstruktur für Tabelle `picture`
 --
 
 CREATE TABLE `picture` (
-  `id` int(6) UNSIGNED NOT NULL,
-  `userid` int(6) UNSIGNED NOT NULL,
+  `id` int(6) NOT NULL,
+  `gallery_id` int(6) NOT NULL,
   `filename` varchar(50) NOT NULL,
-  `picturetitle` varchar(50) NOT NULL,
-  `picturedescription` varchar(150) DEFAULT NULL
+  `name` varchar(50) NOT NULL,
+  `description` varchar(150) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -41,22 +54,36 @@ CREATE TABLE `picture` (
 --
 
 CREATE TABLE `user` (
-  `id` int(6) UNSIGNED NOT NULL,
+  `id` int(6) NOT NULL,
   `email` varchar(50) NOT NULL,
   `username` varchar(16) NOT NULL,
   `password` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Daten für Tabelle `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `username`, `password`) VALUES
+(1, 'user@picsuh.uk.co', 'user', '86f7e437faa5a7fce15d1ddcb9eaeaea377667b8');
+
+--
 -- Indizes der exportierten Tabellen
 --
+
+--
+-- Indizes für die Tabelle `gallery`
+--
+ALTER TABLE `gallery`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indizes für die Tabelle `picture`
 --
 ALTER TABLE `picture`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `userid` (`userid`);
+  ADD KEY `gallery_id` (`gallery_id`);
 
 --
 -- Indizes für die Tabelle `user`
@@ -69,24 +96,35 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT für Tabelle `gallery`
+--
+ALTER TABLE `gallery`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT für Tabelle `picture`
 --
 ALTER TABLE `picture`
-  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT für Tabelle `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- Constraints der exportierten Tabellen
 --
 
 --
+-- Constraints der Tabelle `gallery`
+--
+ALTER TABLE `gallery`
+  ADD CONSTRAINT `gallery_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON UPDATE CASCADE;
+
+--
 -- Constraints der Tabelle `picture`
 --
 ALTER TABLE `picture`
-  ADD CONSTRAINT `picture_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `user` (`id`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `picture_ibfk_1` FOREIGN KEY (`gallery_id`) REFERENCES `gallery` (`id`) ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
