ee4caad7222bb021484d9809677b824f7cac4226
Lüg Blaiger <luc.balsiger@bwdbern.ch>
2017-04-21 08:22:44 +0200

    Picture Upload Update
    Upload of gifs is now possible

--- controller/galleryController.php
+++ controller/galleryController.php
@@ -45,7 +45,7 @@ class galleryController
             $file_type = $_FILES['image']['type'];
             $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
 
-            $expensions= array("jpeg","jpg","png");
+            $expensions= array("jpeg","jpg","png", "gif");
 
             if(in_array($file_ext,$expensions)=== false){
                 $errors[]="extension not allowed, please choose a JPEG or PNG file.";
